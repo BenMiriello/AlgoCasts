2ebc3ec54856c4f219bf7d2b2fbb9a3b4a38a4fb
BenMiriello <benmiriello@gmail.com>
2020-03-23 21:33:42 -0400

    solution (iterative): vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    
+};
 
 module.exports = vowels;
+
+// function vowels(str) {
+//     const array = str.toLowerCase().split('')
+//     const vowels = ['a','e','i','o','u']
+//     let n = 0
+//     for (char = 0; char < array.length; char++) {
+//         for (vowel = 0; vowel < vowels.length; vowel++) {
+//             if (array[char] == vowels[vowel]) {
+//                 n += 1;
+//             };
+//         };
+//     };
+//     return n;
+// };
