cfffa804aec2bc42d3e9a5804229e0cb5592da13
BenMiriello <benmiriello@gmail.com>
2020-03-27 11:32:20 -0400

    Partial solution: matrix 0.03

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -32,7 +32,9 @@ function matrix(n) {
     // note: direction run refers how many times we'll add a number while going in the same direction before changing directions
 
     // count is for 1 - n, iterating through whole thing
+
     // coords is [x,y] or [position in array, index of array in outer]
+
     // dir[0] is direction (0->right 1->down 2->left 3->up)
     // dir[1] is either true or false (true-> do another run through at current length before changing directions, false->length of direction run decrements at end of run)
     // dir[2] is how long this direction has been counting for (0 idxed)
@@ -52,37 +54,44 @@ function matrix(n) {
 
         // when we need to change directions, evaluate
         if (dir[2] === dir[3]) {
-            // decrement direction run length if it's the end of the second run
-            if (dir[1]) { dir[3] -- } ;
             
-            // 
+            // decrement direction run length if it's the end of the second run
+            if (dir[1]) { 
+                dir[3] -- 
+            };
+            
+            // toggle bool that keeps track of when to decrement run length
             dir[1] = !dir[1] ;
 
-            //
+            // reset direction run counter
             dir[2] = 0 ;
 
-            //
+            // set next direction
             if (dir[0] === 3) {
                 dir[0] = 0
             } else {
                 dir[0] ++
             };
+
+            // otherwise just increment run counter
+        } else {
+            dir[2]++
         };
 
+        // set next coords
         switch (dir[0]) {
-            case 0:
-
-                if (count <= n) {
-                    dir 
-                }
-            case 1:
-                
-            case 2:
-                
-            case 3:
-                
+            case 0: coords[0] ++ 
+            case 1: coords[1] ++
+            case 2: coords[0] --
+            case 3: coords[1] --
         };
-        
+
+        fillMatrices(outer, count, coords, dir)
+    };
+};
+
+module.exports = matrix;
+
         // // if () {
         //     if (dir[1] === 1) {
         //         dir[0] = 0
@@ -90,12 +99,3 @@ function matrix(n) {
 
         //     } else dir[0] = 1
         // // }
-
-        // work out next coordinates
-
-        // fillMatrices(outer, count, coords, dir)
-    };
-};
-
-module.exports = matrix;
-
