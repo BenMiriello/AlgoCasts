aa8e6f428b5668414052b3ecd14f3c57119baa65
BenMiriello <benmiriello@gmail.com>
2020-03-29 14:39:52 -0400

    Solution (Stephen's): queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -9,9 +9,18 @@
 //     q.remove(); // returns 1;
 
 class Queue {
-    constructor(){ this.arr = [] }
-    add = (n) => this.arr.push(n)
-    remove = () => this.arr.shift()
+    constructor() {
+        this.data = []
+    };
+
+    add(record) {
+        this.data.unshift(record)
+    }
+
+    remove() {
+        return this.data.pop()
+    }
+
 }
 
 // module.exports = Queue;
@@ -30,33 +39,9 @@ console.log('Order of elements is maintained',
     q.remove() === 3
 )
 
-// test('Queue is a class', () => {
-//     expect(typeof Queue.prototype.constructor).toEqual('function');
-//   });
-  
-//   test('can add elements to a queue', () => {
-//     const q = new Queue();
-//     expect(() => {
-//       q.add(1);
-//     }).not.toThrow();
-//   });
-  
-//   test('can remove elements from a queue', () => {
-//     const q = new Queue();
-//     expect(() => {
-//       q.add(1);
-//       q.remove();
-//     }).not.toThrow();
-//   });
 
-//   test('Order of elements is maintained', () => {
-//     const q = new Queue();
-//     q.add(1);
-//     q.add(2);
-//     q.add(3);
-//     expect(q.remove()).toEqual(1);
-//     expect(q.remove()).toEqual(2);
-//     expect(q.remove()).toEqual(3);
-//     expect(q.remove()).toEqual(undefined);
-//   });
-
+// class Queue {
+//     constructor(){ this.arr = [] }
+//     add = (n) => this.arr.push(n)
+//     remove = () => this.arr.shift()
+// }
