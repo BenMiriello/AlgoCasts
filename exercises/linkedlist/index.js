0f26a22923130e854c4879c31d0717c42879a625
BenMiriello <benmiriello@gmail.com>
2020-03-29 17:25:17 -0400

    LinkedList: define node && llist.insertFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,26 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(){
+        this.head = {data: null, next: null}
+    }
+
+    insertFirst(node){
+        this.head.data = node
+    }
+}
+
+l = new LinkedList
+l.insertFirst(1)
+console.log(l.head.data)
 
 module.exports = { Node, LinkedList };
+
