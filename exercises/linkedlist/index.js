a9dc08945371750e27bc8f67e8dfbc1ea4f3e5d5
BenMiriello <benmiriello@gmail.com>
2020-04-01 20:14:39 -0400

    LinkedList: for...of + generator / iterator

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -106,6 +106,14 @@ class LinkedList {
             node = node.next;
         }
     }
+
+    *[Symbol.iterator](){
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node.next;
+        }
+    }
 };
 
 module.exports = { Node, LinkedList };
