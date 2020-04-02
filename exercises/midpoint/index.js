7c57c011dc8f1fdb1cf07aee40295942468da5a4
BenMiriello <benmiriello@gmail.com>
2020-04-02 10:37:45 -0400

    midpoint: stephen's solution

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -27,3 +27,16 @@ function midpoint(list) {
 }
 
 module.exports = midpoint;
+
+/**
+ * function midpoint(list){
+ *      let slow = list.getFirst();
+ *      let fast = list.getFirst();
+ * 
+ *      while (fast.next && fast.next.next){
+ *          slow = slow.next;
+ *          fast = fast.next.next;
+ *      }
+ *      return slow
+ * }
+ */
