0f9fd0275cf9594640cf3a18210688ae49bfeb71
BenMiriello <benmiriello@gmail.com>
2020-04-07 12:06:08 -0400

    levelwidth: initial solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,26 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let totals = [0]
+  let nodes = [root,'s']
+
+  let node = nodes[0]
+  while (node) {
+    if (node === 's') {
+      if (nodes.length < 2) {
+        return totals
+      }
+      nodes.push(nodes.shift())
+      totals.push(0)
+    } else {
+      nodes.push(...node.children)
+      nodes.shift()
+      totals[totals.length - 1]++
+    }
+    node = nodes[0]
+  }
+}
 
 module.exports = levelWidth;
+
