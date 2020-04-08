09e4f8217307c683f6eb9e10bb2c955a7fa26b1b
BenMiriello <benmiriello@gmail.com>
2020-04-08 12:10:22 -0400

    bst: initial solution

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,44 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data = null){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  contains(data){
+    console.log(data)
+    let node = this;
+    let i = 0
+    while (node != null) {
+      if (node.data === data) return node
+      if (node.data > data) {
+        node = node.left
+      } else if (node.data < data) {
+        node = node.right
+      }
+    }
+    return null
+  }
+
+  insert(data){
+    if (this.data > data) {
+      if (this.left){
+        this.left.insert(data)
+      } else {
+        this.left = new Node(data)
+      }
+    }
+    if (this.data < data) {
+      if (this.right){
+        this.right.insert(data)
+      } else {
+        this.right = new Node(data)
+      }
+    }
+  }
+}
 
 module.exports = Node;
